427147543dec7af5373ac6769b4cfcfdf08a76c3
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-06-14 21:56:18 -0300

    add

---
+++ Projeto AUA/src/controller/controller.js
@@ -0,0 +1,59 @@
+const Usuario = require('../models/usuario');
+
+function indexView(req, res){
+    res.render('index.html');
+}
+
+function logarView(req, res){
+    res.render('logar.html');
+}
+
+function criarContaView(req, res){
+    res.render('usuario_cadastro.html'); 
+}
+
+function animaisView(req,res) {
+    res.render('animais.html');
+}
+
+function cadastrarUsuario(req, res){
+    let usuario = {
+        email: req.body.email,
+        senha: req.body.senha,
+        perfil: req.body.perfil
+    };
+
+    Usuario.create(usuario).then(() => {
+        res.redirect('/?cadastrar_usuario=true');
+    }).catch((err) => {
+        console.log(err);
+        res.redirect('/?cadastrar_usuario=false');
+    });
+}
+
+
+function acessarUsuario(req, res) {
+    const { email, senha } = req.body;
+    
+    Usuario.findOne({ where: { email: email } }).then(usuario => {
+        if (usuario && usuario.senha === senha) {
+            // res.send('Login realizado com sucesso!');
+            res.redirect('/?logar_conta=true');
+        } else {
+            // res.status(401).send('Credenciais inválidas');
+            res.redirect('/?logar_conta=false');
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send('Erro no servidor');
+    });
+}
+
+module.exports = {
+    indexView, 
+    criarContaView, 
+    cadastrarUsuario,
+    acessarUsuario,
+    logarView,
+    animaisView
+}

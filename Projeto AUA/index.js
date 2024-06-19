437d7907a1db943b1e25085a2251563a28478fe7
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-06-19 20:55:33 -0300

    concertando partes da ui na animais.html e animaisDetalhes.html

--- Projeto AUA/index.js
+++ Projeto AUA/index.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(session({
     secret: 'secret-token',
     name: 'sessionId',  
-    resave: false,
+    resave: false, 
     saveUninitialized: false, 
     cookie: { secure: false }
 }))
